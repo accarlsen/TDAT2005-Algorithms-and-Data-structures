b8610d4da0a7546b8b0a630c2dafc332d76dfc71
Name <alexac@student.ntnu.no>
2019-08-19 14:17:29 +0200

    1Asignment Added comments

--- 1Asignment-StocksExample/src/App.java
+++ 1Asignment-StocksExample/src/App.java
@@ -19,5 +19,15 @@ public class App {
         } while(end.getTime()-start.getTime() < 1000);
         time = (double)(end.getTime()-start.getTime())/rounds;
         System.out.println("Milliseconds per round: " + time);
+
+
+        /*
+        Array of 100:    0.0018721087620306388
+        Array of 1000:   0.15673981191222572
+        Array of 10000: 15.363636363636363
+
+        Corresponds to the fact that O( n^2 )
+
+        */
     }
 }
