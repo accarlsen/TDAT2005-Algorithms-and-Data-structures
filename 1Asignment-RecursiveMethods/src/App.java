7180662d66a476c5504fb5267dcf6816b20e3088
Name <alexac@student.ntnu.no>
2019-08-23 16:32:11 +0200

    2Asignment task 3 done (completed)

--- 1Asignment-RecursiveMethods/src/App.java
+++ 1Asignment-RecursiveMethods/src/App.java
@@ -5,6 +5,8 @@ public class App {
         Pow p = new Pow();
         Pow2 p2 = new Pow2();
         
+        //Using Pow class' method, task 2.1-1
+
         Date start = new Date();
         int rounds = 0;
         double time;
@@ -17,10 +19,9 @@ public class App {
         } while(end.getTime()-start.getTime() < 1000);
         time = (double)(end.getTime()-start.getTime())/rounds;
         System.out.println("Milliseconds per round p: " + time);
-        
-        
         System.out.println(p.solve(2, 4));
 
+        //Using Pow2 class' method, task 2.2-3
 
         Date start1 = new Date();
         int rounds1 = 0;
@@ -37,5 +38,24 @@ public class App {
         
         
         System.out.println(p2.solve(2, 4));
+
+        //Comparing to Java's Math.pow method
+
+        Date start2 = new Date();
+        int rounds2 = 0;
+        double time2;
+        Date end2;
+
+        do{
+            double r = Math.pow(2,4);
+            end2 = new Date();
+            rounds2++;
+        } while(end2.getTime()-start2.getTime() < 1000);
+        time2 = (double)(end2.getTime()-start2.getTime())/rounds2;
+        System.out.println("Milliseconds per round p2: " + time2);
+        
+        
+        System.out.println(Math.pow(2, 4));
+
     }
 }
