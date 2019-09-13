718d30ce6ad6d7790f247e3cf123f02da6709203
Name <alexac@student.ntnu.no>
2019-09-13 11:10:55 +0200

    Task1 solved

--- 4Asignment-Trees,Nodes/src/App.java
+++ 4Asignment-Trees,Nodes/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        
+        Josephus j = new Josephus();
+        int result;
+        
+        Date start = new Date();
+        int rounds = 0;
+        double time;
+        Date end;
+        do{
+            result = j.kill(1000000,4);
+            end = new Date();
+            rounds++;
+        } while(end.getTime()-start.getTime() < 1000);
+        time = (double)(end.getTime()-start.getTime())/rounds;
+        System.out.println(time);
+        System.out.println(result);
     }
 }
