8c15e68b191978c2b87b425c02ba96ed199ee59f
Name <alexac@student.ntnu.no>
2019-10-06 10:03:41 +0200

    Asignment7 solved

---
+++ 7Asignment-BreadthFirst&TopographicSearch/src/App.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        
+        File file = new File("D:/AlexanderSkole/NTNU 2019Høst/Algoritmer og Datastrukturer/TDAT2005-Algorithms-and-Data-structures/7Asignment-BreadthFirst&TopographicSearch/src/L7g5");
+        UnweightedGraphs ug = new UnweightedGraphs(file);
+        
+        ug.breadthFirstSearch(5);
+        ug.printUG();
+
+        ArrayList<Integer> ai = ug.topologicalSort();
+        for(int i = ai.size()-1; i >= 0; i--){
+            System.out.println(ai.get(i));
+        }
+    }
+}
